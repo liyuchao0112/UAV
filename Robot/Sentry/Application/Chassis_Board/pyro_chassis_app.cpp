ebd82a362dbbf55feb91ff9c8970884746b2af1a
coaster <2038429915@qq.com>
2026-07-11 22:34:02 +0800

    架构

---
+++ Robot/Sentry/Application/Chassis_Board/pyro_chassis_app.cpp
@@ -0,0 +1,14 @@
+#include "pyro_core_config.h"
+#include "pyro_core_def.h"
+
+#if BOARD == CHASSIS_BOARD
+
+#include "pyro_module_base.h"
+#include "pyro_mutex.h"
+#include "pyro_dr16_rc_drv.h"
+#include "pyro_rc_base_drv.h"
+#include "pyro_com_canrx.h"
+
+
+
+#endif
---
+++ Robot/Sentry/Modules/Chassis/pyro_rudder_chassis.h
@@ -0,0 +1,16 @@
+#ifndef PYRO_RUDDER_CHASSIS_H
+#define PYRO_RUDDER_CHASSIS_H
+
+#include "pyro_algo_pid.h"
+#include "pyro_module_base.h"
+#include "pyro_kin_rudder.h"
+#include "pyro_motor_base.h"
+#include "pyro_ins.h"
+#include "pyro_power_control_drv.h"
+#include "rudder_config.h"
+
+namespace pyro {
+
+} // namespace pyro
+
+#endif
